052bdf1d7917a6fc853a990eda4b435d2e4c10d7
TLukasTNeu <ovonear@gmail.com>
2024-07-19 09:20:24 +0200

    +

--- medusa-config.js
+++ medusa-config.js
@@ -28,8 +28,15 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
+
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -77,9 +84,10 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   database_extra: { ssl: { rejectUnauthorized: false } },
+  redis_url: REDIS_URL
 
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
